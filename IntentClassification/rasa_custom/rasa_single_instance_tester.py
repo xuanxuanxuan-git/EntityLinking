c9c831cd3cb8a21403b8e7ce58d9ec83dcd03ed3
Yueqing <yueqingx@student.unimelb.edu.au>
2021-12-03 11:37:44 +1100

    update input and output method

--- IntentClassification/rasa_custom/rasa_single_instance_tester.py
+++ IntentClassification/rasa_custom/rasa_single_instance_tester.py
@@ -8,18 +8,26 @@
 #
 # To start a RASA Server, please run the below command
 # rasa run --enable-api -m models/20201123-111915.tar.gz
-# rasa shell nlu -m models/20201123-111915.tar.gz
 # Please replace the above command with the appropriate trained model
 
 import requests
 import json
 import os
 import sys
+import socket
+import time
 # Represents the absolute path of the parent folder (IntentClassification)
-rootpath = 'C:/Users/62572/Desktop/COMP90055/IntentClassifier/IntentClassification'
+rootpath = '/mnt/c/Users/62572/Desktop/COMP90055/IntentClassifier/IntentClassification'
+# reachy_path = '/mnt/c/Users/62572/Desktop/COMP90055/IntentClassifier/reachy'
+intent_input_path = '/mnt/c/Users/62572/Desktop/COMP90055/IntentClassifier/IntentClassification/rasa_custom' \
+                    '/intent_input.txt'
+intent_prediction_path = '/mnt/c/Users/62572/Desktop/COMP90055/IntentClassifier/IntentClassification/rasa_custom' \
+                         '/intent_output.txt'
 sys.path.append(rootpath)
+# sys.path.append(reachy_path)
 
 from util.apputil import RASA_SERVER
+
 
 headers = {
     'Content-type': 'application/json'
@@ -47,11 +55,42 @@ def post_to_rasa(command):
     return intent, confidence, intent_ranking
 
 
+# write the prediction results to a file
+def write_prediction_to_file(command, output_path):
+    intent_prediction = ''
+    intent_prediction, _, _ = post_to_rasa(command)
+    with open(output_path, "w") as f:
+        f.write(intent_prediction)
+        f.close()
+    return intent_prediction
+
+
+# read the language and visual data from a file
+def read_intent_input(input_file_path, output_path):
+    command = ''
+    while True:
+        if os.path.isfile(input_file_path):
+            f = open(input_file_path, "r")
+            command = f.readline()
+            prediction = write_prediction_to_file(command, output_path)
+            f.close()
+            os.remove(input_file_path)
+            break
+        else:
+            # print("not found")
+            time.sleep(0.1)
+    return True
+
+
 # Sample test instances
+if __name__ == "__main__":
 
-# With only language data
-# post_to_rasa('hang a right at the wooden dresser and walk to the brown chair ahead')
+    # language data
+    post_to_rasa('pick up the phone and then put it down')
 
-# With both language and visual data
-# post_to_rasa('pick up the credit card on the table @@@@@@ 0.21 3.76 6.87 0.87')
-# post_to_rasa('go up to the table in front of you pick up the credit card to the left of the cardboard box on the table turn to your left and walk into the living room then turn to the first arm chair on your right @@@@@@ 0.79 2.0 2.67 0.978')
+    # With both language and visual data
+    post_to_rasa('pick up the credit card on the table @@@@@@ 0.21 3.76 6.87 0.87')
+    post_to_rasa('go up to the table in front of you pick up the credit card to the left of the cardboard box on the table turn to your left and walk into the living room then turn to the first arm chair on your right @@@@@@ 0.79 2.0 2.67 0.978')
+
+    # read the command message from a file
+    # read_intent_input(intent_input_path, intent_prediction_path)
